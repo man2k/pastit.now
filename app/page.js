397fce8db93b169d26d732f2934db1ff1760f3c2
man2k <mankarmakar4@gmail.com>
2023-05-28 05:07:38 +0530

    Added SunEditor, formated

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
-import Image from "next/image";
 import TextField from "@/components/TextField";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-around p-24 bg-green-300">
-      <div>
+    <main className="flex min-h-screen flex-col items-center justify-evenly bg-green-300">
+      <div className="text-mono mb-0">
         <label className=" font-mono text-4xl font-bold">PASTIT.NOW</label>
       </div>
-      <TextField />
+      <div className="w-full p-16">
+        <TextField />
+      </div>
     </main>
   );
 }
--- components/TextField.jsx
+++ components/TextField.jsx
@@ -1,56 +1,180 @@
 "use client";
-import { useState, useCallback } from "react";
 
-import { Editor, Transforms } from "slate";
-import { createEditor } from "slate";
-import { Slate, Editable, withReact } from "slate-react";
-import Leaf from "./Leaf";
-import DefaultElement from "./DefaultElement";
-import CodeElement from "./CodeElement";
+import React, { useState, useRef, useEffect } from "react";
+import "suneditor/dist/css/suneditor.min.css"; // Import Sun Editor's CSS File
+import dynamic from "next/dynamic";
+import plugins from "suneditor/src/plugins";
+// import { buttonList } from "suneditor-react";
 
-const initialValue = [
-  {
-    type: "paragraph",
-    children: [{ text: "A line of text in a paragraph." }],
-  },
-];
+const SunEditor = dynamic(() => import("suneditor-react"), {
+  ssr: false,
+});
 
 const TextField = () => {
-  const [editor] = useState(() => withReact(createEditor()));
+  const [content, setContent] = useState("");
+  const editorRef = useRef(null);
 
-  const renderElement = useCallback((props) => {
-    switch (props.element.type) {
-      case "code":
-        return <CodeElement {...props} />;
-      default:
-        return <DefaultElement {...props} />;
+  useEffect(() => {
+    if (editorRef.current) {
+      // Access the SunEditor instance and activate all plugins
+      const editor = editorRef.current.editor;
+      editor.getCore().plugins.forEach((plugin) => {
+        editor.addPlugin(plugin);
+      });
     }
   }, []);
 
+  // const handleChange = (value) => {
+  //   // console.log(value);
+  //   setContent(value);
+  // };
+
   return (
-    <Slate editor={editor} initialValue={initialValue}>
-      <Editable
-        renderElement={renderElement}
-        onKeyDown={(event) => {
-          if (event.key === "`" && event.ctrlKey) {
-            event.preventDefault();
-            // Determine whether any of the currently selected blocks are code blocks.
-            const [match] = Editor.nodes(editor, {
-              match: (n) => n.type === "code",
-            });
-            // Toggle the block type depending on whether there's already a match.
-            Transforms.setNodes(
-              editor,
-              { type: match ? "paragraph" : "code" },
-              {
-                match: (n) => Element.isElement(n) && Editor.isBlock(editor, n),
-              }
-            );
-          }
-        }}
-      />
-    </Slate>
+    <SunEditor
+      ref={editorRef}
+      defaultValue={content}
+      setContents={content}
+      onChange={setContent}
+      lang="en"
+      height="auto"
+      width="auto"
+      plugins={Object.keys(plugins)}
+      setOptions={{
+        code: "en",
+        buttonList: [
+          // Default
+          ["undo", "redo"],
+          ["font", "fontSize", "formatBlock"],
+          ["paragraphStyle", "blockquote"],
+          ["bold", "underline", "italic", "strike", "subscript", "superscript"],
+          ["fontColor", "hiliteColor", "textStyle"],
+          ["removeFormat"],
+          ["outdent", "indent"],
+          ["align", "horizontalRule", "list", "lineHeight"],
+          ["table", "link", "image", "video", "audio"],
+          ["imageGallery"],
+          ["fullScreen", "showBlocks", "codeView"],
+          ["preview", "print"],
+          ["save", "template"],
+          ["-left", "#fix", "dir_ltr", "dir_rtl"],
+          // (min-width:992px)
+          [
+            "%992",
+            [
+              ["undo", "redo"],
+              [
+                ":p-More Paragraph-default.more_paragraph",
+                "font",
+                "fontSize",
+                "formatBlock",
+                "paragraphStyle",
+                "blockquote",
+              ],
+              ["bold", "underline", "italic", "strike"],
+              [
+                ":t-More Text-default.more_text",
+                "subscript",
+                "superscript",
+                "fontColor",
+                "hiliteColor",
+                "textStyle",
+              ],
+              ["removeFormat"],
+              ["outdent", "indent"],
+              ["align", "horizontalRule", "list", "lineHeight"],
+              ["-right", "dir"],
+              [
+                "-right",
+                ":i-More Misc-default.more_vertical",
+                "fullScreen",
+                "showBlocks",
+                "codeView",
+                "preview",
+                "print",
+                "save",
+                "template",
+              ],
+              [
+                "-right",
+                ":r-More Rich-default.more_plus",
+                "table",
+                "link",
+                "image",
+                "video",
+                "audio",
+                "math",
+                "imageGallery",
+              ],
+            ],
+          ],
+          // (min-width:768px)
+          [
+            "%768",
+            [
+              ["undo", "redo"],
+              [
+                ":p-More Paragraph-default.more_paragraph",
+                "font",
+                "fontSize",
+                "formatBlock",
+                "paragraphStyle",
+                "blockquote",
+              ],
+              [
+                ":t-More Text-default.more_text",
+                "bold",
+                "underline",
+                "italic",
+                "strike",
+                "subscript",
+                "superscript",
+                "fontColor",
+                "hiliteColor",
+                "textStyle",
+                "removeFormat",
+              ],
+              [
+                ":e-More Line-default.more_horizontal",
+                "outdent",
+                "indent",
+                "align",
+                "horizontalRule",
+                "list",
+                "lineHeight",
+              ],
+              [
+                ":r-More Rich-default.more_plus",
+                "table",
+                "link",
+                "image",
+                "video",
+                "audio",
+                "math",
+                "imageGallery",
+              ],
+              ["-right", "dir"],
+              [
+                "-right",
+                ":i-More Misc-default.more_vertical",
+                "fullScreen",
+                "showBlocks",
+                "codeView",
+                "preview",
+                "print",
+                "save",
+                "template",
+              ],
+            ],
+          ],
+        ],
+        plugins: plugins,
+      }}
+      setDefaultStyle="font-family: cursive; font-size:16px;"
+      // disable={loading}
+      // handleImageUploadBefore={handleImageUploadBefore}
+      // onImageUpload={handleImageUpload}
+      setAllPlugins={true}
+    />
   );
 };
-
 export default TextField;
